29e33770b5ceb3bf748a10473f4233cc307a681a
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-03-10 15:33:14 +0600

    Koin added

---
+++ app/src/main/java/com/example/mymessangerfcm/App.kt
@@ -0,0 +1,34 @@
+package com.example.mymessangerfcm
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            // use AndroidLogger as Koin Logger - default Level.INFO
+            androidLogger()
+
+            // use the Android context given there
+            androidContext(this@App)
+
+            // load properties from assets/koin.properties file
+            androidFileProperties()
+
+            // module list
+            modules(offlineWeatherApp)
+        }
+    }
+
+    val offlineWeatherApp: Module = module {
+
+    }
+}
