9c31aba1305f2fd2c870572dde954778871e65ff
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-04-19 15:35:42 +0600

    chat fragment toolbar implemented

--- app/src/main/java/com/example/mymessangerfcm/chat/ChatFragment.kt
+++ app/src/main/java/com/example/mymessangerfcm/chat/ChatFragment.kt
@@ -7,22 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import coil.api.load
 import coil.transform.CircleCropTransformation
+import com.example.mymessangerfcm.MessangerViewModel
 import com.example.mymessangerfcm.R
+import com.example.mymessangerfcm.databinding.FragmentChatBinding
+import com.example.mymessangerfcm.databinding.FragmentChatChannelBinding
 import kotlinx.android.synthetic.main.fragment_chat.*
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class ChatFragment : Fragment() {
+    private lateinit var viewDataBinding: FragmentChatBinding
+    val messangerViewModel: MessangerViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        viewDataBinding = FragmentChatBinding.inflate(inflater, container, false).apply {
+            viewModel = messangerViewModel
+        }
+        return viewDataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        avatar_group.load(R.drawable.ic_launcher_foreground){
+        avatar_group.load(R.mipmap.ava){
             transformations(CircleCropTransformation())
+        }
+        backButton.setOnClickListener {
+            requireActivity().onBackPressed()
         }
     }
 }
